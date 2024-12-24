cfb06a3c7b96d4586fb5353d3f525bc2d616fbfb
我叫以赏 <422880152@qq.com>
2024-12-24 10:23:39 +0800

    Fix: 替换弃用的方法
    可能是打错了？prompt 中没有 json_output_prompt_getter 方法，应该是 format_prompt_getter

--- response_format/__init__.py
+++ response_format/__init__.py
@@ -2,10 +2,10 @@ from response_format.parser import (
     parse_content_to_json,
     parse_model_to_json
 )
-from response_format.prompt import json_output_prompt_getter
+from response_format.prompt import format_prompt_getter
 
 __all__ = (
     "parse_content_to_json",
     "parse_model_to_json",
-    "json_output_prompt_getter"
+    "format_prompt_getter"
 )
